893c3733139e8bf49084325dde25ca8b0eca0833
Nick Quinlan <nicholas.quinlan@sendgrid.com>
2014-06-09 14:46:43 -0700

    add prep file

---
+++ prep.sh
@@ -0,0 +1,4 @@
+cf unbind-service sendgrid-demo sendgrid-is-cool
+open https://console.run.pivotal.io/organizations/f9bb9611-cd72-4a74-a0ed-8c03775196ef/spaces/5476ff4b-7732-4394-a1bc-1bebc5c89c67
+open https://sendgrid.com/developer/reply
+open https://nqsgdemo-event-handler.jit.su/
