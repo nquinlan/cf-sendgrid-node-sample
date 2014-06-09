9fa4ff40265e3f89f88325cb4762f82ed812935e
Nick Quinlan <nicholas.quinlan@sendgrid.com>
2014-06-09 11:55:10 -0700

    demo completed

--- app.js
+++ app.js
@@ -4,12 +4,26 @@ var multipart = require('connect-multiparty');
 
 app.use(multipart());
 
+var bird = require("./bird");
 
+var services = JSON.parse(process.env.VCAP_SERVICES);
+
+var sendgridService = services.sendgrid[0];
+var sendgrid = require('sendgrid')(sendgridService.credentials.username, sendgridService.credentials.password);
 
 app.all('/', function (req, res) {
 	
+	bird(req.body.subject, function (b) {
+		sendgrid.send({
+			to: req.body.from,
+			from: "nick@sendgrid.com",
+			subject: "Thanks for watching how to use Cloud Foundry and SendGrid 👍",
+			html: b.info
+		}, function (err, json) {
+			res.send(json);
+		});
+	});
 
-	
 });
 
 var server = app.listen(process.env.VCAP_APP_PORT || 3000, function () {
