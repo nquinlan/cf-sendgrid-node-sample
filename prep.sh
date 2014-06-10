d0db08e4635df095f5b3c62e9fe01f8d08bcf2d4
Nick Quinlan <nicholas.quinlan@sendgrid.com>
2014-06-10 10:11:46 -0700

    updated prep.sh

--- prep.sh
+++ prep.sh
@@ -2,3 +2,8 @@ cf unbind-service sendgrid-demo sendgrid-is-cool
 open https://console.run.pivotal.io/organizations/f9bb9611-cd72-4a74-a0ed-8c03775196ef/spaces/5476ff4b-7732-4394-a1bc-1bebc5c89c67
 open https://sendgrid.com/developer/reply
 open https://nqsgdemo-event-handler.jit.su/
+open /Applications/Stickies.app
+git checkout -b cf-summit
+subl .
+subl app.js
+printf "🌉" | pbcopy
